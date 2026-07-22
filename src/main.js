bbb3c458688ffb71da42f22cae9b8d54ffd090b1
mhj1017 <2025040220@bistu.edu.cn>
2026-07-22 13:46:22 +0800

    B：前端页面完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+
+Vue.use(ElementUI)
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:5000/api'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
